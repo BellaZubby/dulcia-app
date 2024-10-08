9cacf071e30df9f9fd75e8244d27897a931e0281
BellaZubby <nwankwoamabella@yahoo.com>
2024-10-08 13:02:28 +0100

    fix

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,58 +1,45 @@
-"use client"
-import React, { Suspense } from 'react'
-import { banner} from '@/app/Hooks/data'
-import HeroSection from '@/app/components/hero/hero'
-import Products from '@/app/components/products'
-import { useSearchParams } from 'next/navigation'
-import SearchResult from '@/app/components/searchResult'
-import School from '@/app/components/school'
-import TestimonialDesktop from '@/app/components/testimonial/testimonialDesktop'
-import useMediaQuery from '@/app/Hooks/useMediaQuery'
-import TestimonialMobile from '@/app/components/testimonial/testimonialMobile'
-import Services from '@/app/components/services'
-import { queryData} from '@/app/Hooks/testData'
-type Props = {
-  
-}
+"use client";
+import React, { Suspense } from "react";
+import { banner } from "@/app/Hooks/data";
+import HeroSection from "@/app/components/hero/hero";
+import Products from "@/app/components/products";
+import { useSearchParams } from "next/navigation";
+import SearchResult from "@/app/components/searchResult";
+import School from "@/app/components/school";
+import TestimonialDesktop from "@/app/components/testimonial/testimonialDesktop";
+import useMediaQuery from "@/app/Hooks/useMediaQuery";
+import TestimonialMobile from "@/app/components/testimonial/testimonialMobile";
+import Services from "@/app/components/services";
+import { queryData } from "@/app/Hooks/testData";
+type Props = {};
 
-const HomePage = ({}:Props) => {
+const HomePage = ({}: Props) => {
   const searchParams = useSearchParams();
-  const query = searchParams.get('query') || '';
+  const query = searchParams.get("query") || "";
   const aboveMediumScreen = useMediaQuery("(min-width:1060px)");
 
-
-  
   return (
-      <Suspense fallback={<div>...Loading</div>}>
-          {query ?
-          
-               <div className='pt-40 ssm:pt-48 sm:pt-48 bm:pt-48 md:pt-48 font-robotoCondensed'>
-              
-                  <SearchResult query={query}/> 
-               
-             
+    <>
+      {query ? (
+        <Suspense fallback={<div>...Loading</div>}>
+          <div className="pt-40 ssm:pt-48 sm:pt-48 bm:pt-48 md:pt-48 font-robotoCondensed">
+            <SearchResult query={query} />
           </div>
-      
-           
-           
-          
-         :
-          <div className='pt-14 ssm:pt-24 sm:pt-32 font-robotoCondensed'>
-            <HeroSection data={banner} />
-            <Products data={queryData} className={''}/>
-            <Services/>
-              {
-                aboveMediumScreen ?  <TestimonialDesktop/> : <TestimonialMobile/>
-              }
-            <School className=''/> 
-          
-           </div>
-           
-         
-          }
-            
         </Suspense>
-  )
-}
+      ) : (
+        <Suspense fallback ={<div>...Loading</div>}>
+          <div className="pt-14 ssm:pt-24 sm:pt-32 font-robotoCondensed">
+          <HeroSection data={banner} />
+          <Products data={queryData} className={""} />
+          <Services />
+          {aboveMediumScreen ? <TestimonialDesktop /> : <TestimonialMobile />}
+          <School className="" />
+        </div>
+        </Suspense>
+        
+      )}
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
